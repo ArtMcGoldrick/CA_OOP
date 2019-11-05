14fc08d9c6f07953c339e52600c4a16d69a4107e
S00186896 <S00186896@LAB-B1042-27>
2019-11-05 16:45:00 +0000

    hello

---
+++ CA_OOP/Book.cs
@@ -0,0 +1,62 @@
+﻿/*************************
+ * NAME: Art Mc Goldrick *
+ * DATE: 5/11/2019       *
+ * DESC: Book            *
+ *************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_OOP
+{
+     //Enum used to create Genre with values
+        public enum Genre
+        {
+            Fiction,
+            Biography,
+            Computing,
+            Literature,
+        }
+    class Book : IComparable
+    {
+       
+
+        //Book properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public int Pages { get; set; }
+        public Genre GetGenre { get; }
+
+        //Construtors
+        public Book(string title, string author, int pages, DateTime published)
+        {
+            Title = title;
+            Author = author;
+            Published = published;
+            Pages = pages;
+        }
+
+        //ToString to display values
+        public override string ToString()
+        {
+            return $"{Title}\t{Author}\t{Pages}\t{Published}\t{GetGenre}";
+        }
+
+        //Used to sort with author first
+        public int CompareTo(object obj)
+        {
+            Book that = (Book)obj;
+            return this.Title.CompareTo(that.Title);           
+        }
+
+        //Used to sort with title first
+        public int CompareTo1(object obj)
+        {
+            Book that = (Book)obj;
+            return this.Author.CompareTo(that.Author);
+        }
+    }
+}
--- CA_OOP/Program.cs
+++ CA_OOP/Program.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿/*************************
+ * NAME: Art Mc Goldrick *
+ * DATE: 5/11/2019       *
+ * DESC: Book            *
+ *************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CA_OOP
 {
@@ -10,6 +17,60 @@ namespace CA_OOP
     {
         static void Main(string[] args)
         {
-        }
+            //Creating List
+            List<Book> books = new List<Book>();
+
+            //Creating 5 book objects
+            Book book1 = new Book("James Joyce", "Ulysses     ", 730, new DateTime(02 / 02 / 1922));
+            Book book2 = new Book("B Kieran", "The programming", 279, new DateTime(28 / 02 / 1978));
+            Book book3 = new Book("Walter Issac", "Steve Jobs ", 627, new DateTime(24 / 10 / 2011));
+            Book book4 = new Book("JRR Tolkien", "The Hobbit  ", 310, new DateTime(21 / 09 / 1937));
+            Book book5 = new Book("James Joyce", "Portrait    ", 299, new DateTime(29 / 12 / 1916));
+
+            //Adding objects into list
+            books.Add(book1);
+            books.Add(book2);
+            books.Add(book3);
+            books.Add(book4);
+            books.Add(book5);
+
+            //Method used to display the book objects
+            Display(books);
+            Console.WriteLine("");
+            //Sorted then displayed with author
+            books.Sort();
+            Display(books);
+
+
+
+        }//end of main method
+
+
+        //Method for displaying list
+        static void Display(List<Book> books)
+        {
+            //Displays the books with spaces         
+            Console.WriteLine("Author          Title           Pages   Publication Date");
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+            
+        }//end of display
+
+        //Method for display with age
+        static void DisplayWithAge(List<Book> books)
+        {
+            int year = 2019;
+
+            //Displays the books with spaces         
+            Console.WriteLine("Author          Title           Pages   Publication Date  Age");
+            foreach (Book book in books)
+            {
+                //int result = DateTime.Parse(book.Published);
+                //Console.WriteLine(book + (year - book.Published));
+            }
+
+        }//end of display with age
     }
 }
